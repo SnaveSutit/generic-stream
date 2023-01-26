6fd75f63c3732b1b3bd3b1760b5e9a0c72ffe2a0
SnaveSutit <snavesutit@gmail.com>
2023-01-26 08:23:45 -0500

    Fine. 🛠️

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import { StringStream } from './stringStream'
-
 export { GenericStream } from './genericStream'
 export { StringStream } from './stringStream'
-
-const test = new StringStream('Hello World')
